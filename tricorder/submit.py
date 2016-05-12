442cf89ecfaae7d6bb3cbafdbd099a2c922c0210
mbaumer <mbaumer12@gmail.com>
2016-05-12 12:12:19 -0700

    can now specify custom job time limits

--- tricorder/submit.py
+++ tricorder/submit.py
@@ -3,13 +3,14 @@ import os, sys
 
 def runall(runname):
 	logdir = '/home/mbaumer/tricorder/tricorder/logs/'
-	runstrs = ['d d d']
-	#runstrs = ['d d d','d r r','r d r','r r d','d d r','d r d','r d d','r r r']
-	for runstr in runstrs:
+	rundict = {'d d d': 1}
+	#rundict = {'d d d': 1, 'd r r': 12,'r d r' : 12,'r r d': 12,'d d r': 2,'d r d' : 2,'r d d' : 2 ,'r r r' : 47}
+	for runstr in rundict.keys():
+		timestr = '#SBATCH --time='+str(rundict[runstr])+':00:00 \n'
 		runchars = runstr.replace(" ","")
 		f = open('temp.sbatch','w')
 		configstrs = ['#!/bin/bash \n', 
-			'#SBATCH --time=24:00:00 \n',
+			timestr,
 			'#SBATCH --qos=iric \n',
 			'#SBATCH --nodes=1 \n',
 			'#SBATCH --mem-per-cpu=4000 \n',
@@ -22,8 +23,9 @@ def runall(runname):
 			'srun /home/mbaumer/anaconda2/bin/python2.7 tricorder.py '+runstr+' '+runname+' \n' ]
 		f.writelines(configstrs)
 		f.close()
-		os.system('sbatch temp.sbatch')
-		os.system('rm temp.sbatch')
+		break
+		#os.system('sbatch temp.sbatch')
+		#os.system('rm temp.sbatch')
 
 if __name__ == '__main__':
 	runall(sys.argv[1])
