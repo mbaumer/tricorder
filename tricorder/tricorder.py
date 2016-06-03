283cb56c1e31bc1dbde0043d0eb4370e6d49b296
mbaumer <mbaumer12@gmail.com>
2016-06-03 15:45:37 -0700

    from local

--- tricorder/tricorder.py
+++ tricorder/tricorder.py
@@ -14,18 +14,29 @@ class NNNHandler (object):
         self.datapath = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_data.fits'
         self.randompath = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_randoms.fits'
         self.outdir = '/scratch/PI/kipac/mbaumer/des/3pt_results/'
+        #write config file
 
-        self.min_r = 1
-        self.max_r = 50
-        self.n_rbins = 11
+        configdict = {}
+        configdict['min_r'] = 1
+        configdict['max_r'] = 50
+        configdict['n_rbins'] = 11
         
-        self.min_u = 0.9
-        self.max_u = 1.0
-        self.n_ubins = 1
+        configdict['min_u'] = 0.9
+        configdict['max_u'] = 1.0
+        configdict['n_ubins'] = 1
         
-        self.min_v = -0.1
-        self.max_v = 0.1
-        self.n_vbins = 1
+        configdict['min_v'] = -0.1
+        configdict['max_v'] = 0.1
+        configdict['n_vbins'] = 1
+
+        self.config = configdict
+
+    def load(self,runname):
+        self.config = None #TODO load config dict!
+        [ddd,ddr,drd,rdd,drr,rdr,rrd,rrr] = 8*[treecorr.NNNCorrelation(min_sep=self.config['min_r'], max_sep=self.config['max_r'], nbins=self.config['n_rbins'], min_u=self.config['min_u'], 
+          max_u=self.config['max_u'], nubins=self.config['n_ubins'], min_v=self.config['min_v'], max_v=self.config['max_v'], nvbins=self.config['n_vbins'], bin_slop=1, sep_units='arcmin')]
+        for nnn in [ddd,ddr,drd,rdd,drr,rdr,rrd,rrr]:
+            #do something
 
     def run(self,set1,set2,set3):
         data = fits.getdata(self.datapath)
@@ -34,9 +45,12 @@ class NNNHandler (object):
         cat = treecorr.Catalog(ra=data['RA'], dec=data['DEC'], ra_units='degrees', dec_units='degrees')
         random_cat = treecorr.Catalog(ra=randoms['RA'], dec=randoms['DEC'], ra_units='degrees', dec_units='degrees')
         setdict = {'d':cat,'r':random_cat}
-        nnn = treecorr.NNNCorrelation(min_sep=self.min_r, max_sep=self.max_r, nbins=self.n_rbins, min_u=self.min_u, 
-          max_u=self.max_u, nubins=self.n_ubins, min_v=self.min_v, max_v=self.max_v, nvbins=self.n_vbins, bin_slop=1, sep_units='arcmin')
+        nnn = treecorr.NNNCorrelation(min_sep=self.config['min_r'], max_sep=self.config['max_r'], nbins=self.config['n_rbins'], min_u=self.config['min_u'], 
+          max_u=self.config['max_u'], nubins=self.config['n_ubins'], min_v=self.config['min_v'], max_v=self.config['max_v'], nvbins=self.config['n_vbins'], bin_slop=1, sep_units='arcmin')
         
+        #TODO:
+        #write out config dict used for processing runname
+
         print 'starting processing!'
         toc = time.time()
         if ((set1 == set2) and (set2 == set3)):
