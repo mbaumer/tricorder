d83e233baaceaba7c06e2952cc8f755bc3a5d116
mbaumer <mbaumer12@gmail.com>
2016-06-13 17:02:20 -0700

    catalog cuts; attributes in dict

--- tricorder/tricorder.py
+++ tricorder/tricorder.py
@@ -8,78 +8,100 @@ import numpy as np
 import time
 import json
 
-class NNNHandler (object):
+outdir = '/scratch/PI/kipac/mbaumer/des/3pt_results/'
 
+class NNNPlotter (object):
     def __init__(self,runname):
-        self.runname = runname
-        self.datapath = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_data.fits'
-        self.randompath = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_randoms.fits'
-        self.outdir = '/scratch/PI/kipac/mbaumer/des/3pt_results/'
-        #write config file
         config_fname = outdir+runname+'.config'
-
-        #if runname is already done, read in results
-        if os.path.exists(config_fname):
+        try: 
             with open(config_fname) as f:
                 configdict = json.loads(f.read())
-        #otherwise, make a new one
-        else:
-            configdict = {}
-            configdict['min_sep'] = 1
-            configdict['max_sep'] = 50
-            configdict['nbins'] = 11
-            
-            configdict['min_u'] = 0.9
-            configdict['max_u'] = 1.0
-            configdict['nubins'] = 1
-            
-            configdict['min_v'] = -0.1
-            configdict['max_v'] = 0.1
-            configdict['nvbins'] = 1
-
-            configdict['bin_slop'] = 0.1
-            configdict['sep_units'] = 'arcmin'
-
-            #rest of this is for other parts of analysis; treecorr doesn't care
-            configdict['min_z'] = .5
-            configdict['max_z'] = .6
-
-            #write it out so we remember what we did
-            f = open(config_fname,'w')
-            f.write(json.dumps(configdict))
-            f.close()
-
-        self.config = configdict
+        except IOError:
+            print 'results for '+runname+' not found in '+outdir
 
     def load(self):
         [ddd,ddr,drd,rdd,drr,rdr,rrd,rrr] = 8*[treecorr.NNNCorrelation(config=self.config)]
         for nnn in [ddd,ddr,drd,rdd,drr,rdr,rrd,rrr]:
-            #do something
+            pass
 
-    def run(self,set1,set2,set3):
-        data = fits.getdata(self.datapath)
-        randoms = fits.getdata(self.randompath)
+class NNNProcessor (object):
+
+    def __init__(self,runname):
+        config_fname = outdir+runname+'.config'
+        assert os.path.exists(config_fname) is False, runname+' is already processed; delete to re-run'
+        
+        configdict = {}
+        #treecorr ignores irrelevant keys
+        configdict['runname'] = runname
+        configdict['datapath'] = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_data.fits'
+        configdict['randompath'] = '/scratch/PI/kipac/mbaumer/des/data/redmagic_sv_randoms.fits'
+
+        configdict['min_z'] = .5
+        configdict['max_z'] = .7
+        #SPT-E footprint
+        configdict['min_ra'] = 60
+        configdict['max_ra'] = 92
+        configdict['min_dec'] = -61
+        configdict['max_dec'] = -40
+
+        configdict['min_sep'] = 9
+        configdict['max_sep'] = 15
+        configdict['nbins'] = 1
+        
+        configdict['min_u'] = 0.375
+        configdict['max_u'] = 0.625
+        configdict['nubins'] = 1
+        
+        configdict['min_v'] = -1
+        configdict['max_v'] = 1
+        configdict['nvbins'] = 15
+
+        configdict['bin_slop'] = 0.1
+        configdict['sep_units'] = 'arcmin'
+
+        #write it out so we remember what we did
+        f = open(config_fname,'w')
+        f.write(json.dumps(configdict))
+        f.close()
+
+        self.config = configdict
+
+    def prepareCatalog(self):
+        data = fits.getdata(self.config['datapath'])
+        randoms = fits.getdata(self.config['randompath'])
+
+        data = data[((data['ZREDMAGIC'] > self.config['min_z']) & (data['ZREDMAGIC'] < self.config['max_z']))]
+        data = data[((data['RA'] > self.config['min_ra']) & (data['RA'] < self.config['max_ra']))]
+        data = data[((data['DEC'] > self.config['min_dec']) & (data['DEC'] < self.config['max_dec']))]
+
+        randoms = randoms[((randoms['Z'] > self.config['min_z']) & (randoms['Z'] < self.config['max_z']))]
+        randoms = randoms[((randoms['RA'] > self.config['min_ra']) & (randoms['RA'] < self.config['max_ra']))]
+        randoms = randoms[((randoms['DEC'] > self.config['min_dec']) & (randoms['DEC'] < self.config['max_dec']))]
 
         cat = treecorr.Catalog(ra=data['RA'], dec=data['DEC'], ra_units='degrees', dec_units='degrees')
         random_cat = treecorr.Catalog(ra=randoms['RA'], dec=randoms['DEC'], ra_units='degrees', dec_units='degrees')
+        return cat, random_cat
+
+    def run(self,set1,set2,set3):
+        cat, random_cat = self.prepareCatalog()
         setdict = {'d':cat,'r':random_cat}
         nnn = treecorr.NNNCorrelation(config=self.config)
-        
-        #TODO:
-        #write out config dict used for processing runname
 
         print 'starting processing!'
         toc = time.time()
+
         if ((set1 == set2) and (set2 == set3)):
+            #this is faster even though it shouldn't in theory...
             nnn.process(setdict[set1])
-            #this saves time even though it shouldn't in theory...
         else:
             nnn.process(setdict[set1],setdict[set2],setdict[set3])
+
         tic = time.time()
         print 'that took', tic-toc
-        fname = self.outdir+self.runname+set1+set2+set3+'.out'
+
+        fname = outdir+self.config['runname']+set1+set2+set3+'.out'
         nnn.write(fname,nnn)
 
 if __name__ == '__main__':
-    handler = NNNHandler(sys.argv[4])
+    handler = NNNProcessor(sys.argv[4])
     handler.run(sys.argv[1], sys.argv[2], sys.argv[3])
