0af578c166b6e240613e76b41e5df8ff18130c64
mbaumer <mbaumer12@gmail.com>
2016-09-09 17:46:04 -0700

    now doing projected 3pt

--- tricorder/tricorder.py
+++ tricorder/tricorder.py
@@ -3,6 +3,7 @@ from __future__ import division
 import sys, os.path
 sys.path.append('/home/mbaumer/anaconda2/bin/')
 from astropy.io import fits
+from astropy.cosmology import Planck15 as cosmo
 import treecorr
 import sys
 import numpy as np
@@ -10,18 +11,21 @@ import time
 import json
 import platform
 
+y1_main = {}
+y1_main['min_ra'] = 0
+y1_main['max_ra'] = 360
+y1_main['min_dec'] = -70
+y1_main['max_dec'] = -35
+
+##User settings!
+footprint = y1_main
 datapath = '/nfs/slac/g/ki/ki19/des/mbaumer/3pt_data/redmagic_'
 outdir = '/nfs/slac/g/ki/ki19/des/mbaumer/3pt_runs/'
+metric = 'Rperp'
 
 #if 'sh-' in platform.node():
 #    datapath = '/scratch/PI/kipac/mbaumer/des/data/redmagic_'
 #    outdir = '/scratch/PI/kipac/mbaumer/des/3pt_results/'
-
-y1_main_footprint = {}
-y1_main_footprint['min_ra'] = 0
-y1_main_footprint['max_ra'] = 360
-y1_main_footprint['min_dec'] = -70
-y1_main_footprint['max_dec'] = -35
 
 class NNNProcessor (object):
 
@@ -35,25 +39,29 @@ class NNNProcessor (object):
 
         configdict['datapath'] = datapath+'data.fits'
         configdict['randompath'] = datapath+'randoms_'+str(self.random_set_id)+'.fits'
-        self.footprint = y1_main_footprint
+
+        for key,value in footprint.iteritems():
+            configdict[key] = value
     
+        configdict['metric'] = metric
+
         configdict['min_z'] = .5
         configdict['max_z'] = .7
 
         configdict['runname'] = runname
-        configdict['min_sep'] = 3
+        configdict['min_sep'] = 1
         configdict['max_sep'] = 25
         configdict['nbins'] = 200
         
-        configdict['min_u'] = 0
+        configdict['min_u'] = .4
         configdict['max_u'] = 1
-        configdict['nubins'] = 100
+        configdict['nubins'] = 60
         
-        configdict['min_v'] = -1
+        configdict['min_v'] = 0
         configdict['max_v'] = 1
-        configdict['nvbins'] = 200
+        configdict['nvbins'] = 100
 
-        configdict['bin_slop'] = 0.1
+        configdict['bin_slop'] = 1
         configdict['sep_units'] = 'arcmin'
 
         self.config = configdict
@@ -73,8 +81,8 @@ class NNNProcessor (object):
             cat = cat[((cat['Z'] > self.config['min_z']) & (cat['Z'] < self.config['max_z']))]
         else:
             print 'one or more input cats have no redshift data'
-        cat = cat[((cat['RA'] > self.footprint['min_ra']) & (cat['RA'] < self.footprint['max_ra']))]
-        cat = cat[((cat['DEC'] > self.footprint['min_dec']) & (cat['DEC'] < self.footprint['max_dec']))]
+        cat = cat[((cat['RA'] > footprint['min_ra']) & (cat['RA'] < footprint['max_ra']))]
+        cat = cat[((cat['DEC'] > footprint['min_dec']) & (cat['DEC'] < footprint['max_dec']))]
         #later, color, magnitude cuts...
         return cat
 
@@ -84,15 +92,21 @@ class NNNProcessor (object):
 
         joint_ra_table = np.hstack([data['RA'],randoms['RA']])
         joint_dec_table = np.hstack([data['DEC'],randoms['DEC']])  
+        joint_z_table = np.hstack([data['ZREDMAGIC'],randoms['Z']])
 
         print 'joint cat has ', joint_ra_table.shape
         print 'randoms have ', len(randoms['RA'])
         
         wt_factor = len(data['RA'])/len(randoms['RA'])
         weights = np.hstack([np.ones_like(data['RA']),-(wt_factor)*np.ones_like(randoms['RA'])])
+        print 'sum of weights (should be close to zero): ', np.sum(weights)
 
-        joint_cat = treecorr.Catalog(ra=joint_ra_table, dec=joint_dec_table, ra_units='degrees', dec_units='degrees', w=weights)
-        random_cat = treecorr.Catalog(ra=randoms['RA'], dec=randoms['DEC'], ra_units='degrees', dec_units='degrees')
+        joint_cat = treecorr.Catalog(ra=joint_ra_table, dec=joint_dec_table, 
+            ra_units='degrees', dec_units='degrees',  
+            r=cosmo.comoving_distance(joint_z_table)/cosmo.h, w=weights)
+        random_cat = treecorr.Catalog(ra=randoms['RA'], dec=randoms['DEC'], 
+            ra_units='degrees', dec_units='degrees',
+            r=cosmo.comoving_distance(randoms['Z'])/cosmo.h)
 
         return joint_cat, random_cat
 
@@ -103,12 +117,23 @@ class NNNProcessor (object):
 
         print 'starting numerator!'
         toc = time.time()
-        nnn.process(joint_cat)
+        nnn.process(joint_cat,metric=metric)
         tic = time.time()
         print 'numerator took', tic-toc
 
         fname = outdir+self.config['runname']+str(self.random_set_id)+'.out'
         nnn.write(fname,file_type='FITS')
+
+         if self.random_set_id == 0:
+            print 'also doing denominator'
+            rrr = treecorr.NNNCorrelation(config=self.config)
+            toc = time.time()
+            rrr.process(random_cat,metric=metric)
+            tic = time.time()
+            print 'denominator took', tic-toc
+            fname = outdir+self.config['runname']+'RRR'+str(self.random_set_id)+'.out'
+            rrr.write(fname,file_type='FITS')
+
 
 if __name__ == '__main__':
     handler = NNNProcessor(sys.argv[1],sys.argv[2])
