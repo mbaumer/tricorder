55afcd785fc1660fe8ced45e73e65462539312ad
mbaumer <mbaumer12@gmail.com>
2017-10-02 14:36:36 -0700

    gotta fix more bugs

--- tricorder/datasets_script.py
+++ tricorder/datasets_script.py
@@ -1,48 +1,22 @@
 #!/u/ki/mbaumer/anaconda/bin/python
 import subprocess
 
-#need args
-cmd_list = ["import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.15,.2,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.3,.35,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.45,.5,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.6,.65,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.15,.18,0,90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.3,.33,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.45,.48,0,90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.6,.63,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.15,.16,0,90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.3,.31,0, 90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.45,.46,0,90,-60,-40)",
-            "import datasets; dset = datasets.DMDataset('/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/dark_matter/dm_cat_2600Mpc_data.fits','/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new/redmagicHD/buzzard-v1.1-y1a1-full_run_redmapper_v6.4.13_redmagic_highdens_0.5-10_vlim_zmask.fit'); dset.make(1024,.6,.61,0, 90,-60,-40)",
-            ]
-
-
 cmd_list = []
-for delta_z in [0.025,0.05,0.1,0.15]:
+oversamp = 5
+
+for delta_z in [0.05,0.1,0.15]:
     z = 0.15
-    while z < .75:
-        cmd_str = "import datasets; dset = datasets.LSSDataset(); dset.make(1024,"+str(z)+","+str(z+delta_z)+",0, 100,-60,-40)"
+    while z + delta_z <= .9:
+        cmd_str = "import datasets; dset = datasets.LSSDataset(); dset.make(1024,"+str(oversamp)+","+str(z)+","+str(z+delta_z)+",0, 100,-60,-40,write=True)"
         cmd_list.append(cmd_str)
+        cmd_str = "import datasets; dset = datasets.DMDataset(); dset.make(1024,"+str(oversamp)+","+str(z)+","+str(z+delta_z)+",0, 100,-60,-40,write=True)"
+        cmd_list.append(cmd_str)
+        if z + delta_z <= .6:
+            cmd_str = "import datasets; dset = datasets.RedmagicDataset(); dset.make(1024,"+str(oversamp)+","+str(z)+","+str(z+delta_z)+",0, 100,-60,-40,write=True)"
+            cmd_list.append(cmd_str)
         z += delta_z 
-    
-"""
-cmd_list = ["import datasets; dset = datasets.LSSDataset(); dset.make(1024,.15,.2,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.3,.35,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.45,.5,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.6,.65,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.15,.18,0,100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.3,.33,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.45,.48,0,100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.6,.63,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.15,.16,0,100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.3,.31,0, 100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.45,.46,0,100,-60,-40)",
-            "import datasets; dset = datasets.LSSDataset(); dset.make(1024,.6,.61,0, 100,-60,-40)",
-            ]
-"""
-
-
 
 for cmd_str in cmd_list:
-    subprocess.call(["bsub", "-W", "08:00", "-R", "rusage[mem=4000]",
+    print cmd_str
+    subprocess.call(["bsub", "-W", "08:00", "-R", "rusage[mem=8000]",
                          "python", "-c", cmd_str])
