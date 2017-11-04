0477bd2521bf1442a12fc9606960e0e4d4c22b1b
mbaumer <mbaumer12@gmail.com>
2017-11-04 10:29:20 -0700

    jk for point 3d

--- tricorder/tricorder_script.py
+++ tricorder/tricorder_script.py
@@ -3,23 +3,25 @@ import sys
 from glob import glob
 import numpy as np
 
-dset_fname_vec = glob(
-    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/redmagicHD/data/ZSPEC*.dset')
-#dset_fname_vec += glob(
+dset_fname_vec = ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/redmagicHD/data/ZSPEC0.15_0.3nside1024nJack30.dset',
+                   '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/redmagicHD/data/ZSPEC0.3_0.45nside1024nJack30.dset',
+                   '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/redmagicHD/data/ZSPEC0.45_0.6nside1024nJack30.dset'
+# dset_fname_vec += glob(
 #    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/lss_sample/data/RED*.dset')
-#dset_fname_vec = glob(
+# dset_fname_vec = glob(
 #    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/dark_matter/data/RED*.dset')
-#dset_fname_vec += glob(
+# dset_fname_vec += glob(
 #    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y1_0_a/halos/data/RED*.dset')
 
-dset_fname_vec += glob(
-    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y3_0_a/redmagicHD/data/ZSPEC*.dset')
-#dset_fname_vec += glob(
+# dset_fname_vec += glob(
+#    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y3_0_a/redmagicHD/data/ZSPEC*.dset')
+# dset_fname_vec += glob(
 #    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/Buzzard_v1.6_Y3_0_a/lss_sample/data/RED*.dset')
 
-dset_fname_vec += glob(
-    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/MICE/redmagicHD/data/ZSPEC*.dset')
-#dset_fname_vec += glob(
+dset_fname_vec += ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/MICE/redmagicHD/data/ZSPEC0.15_0.3nside1024nJack30.dset',
+                   '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/MICE/redmagicHD/data/ZSPEC0.3_0.45nside1024nJack30.dset',
+                   '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/MICE/redmagicHD/data/ZSPEC0.45_0.6nside1024nJack30.dset', ]
+# dset_fname_vec += glob(
 #    '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/MICE/dark_matter/data/RED*.dset')
 
 print len(dset_fname_vec)
@@ -28,19 +30,21 @@ print len(dset_fname_vec)
 #'/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test2_proj_rpar100.config',
 #'/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test2_3d.config',
 
-config_fname_pointvec = []
-#config_fname_pointvec = ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test3_3d.config',
+# config_fname_pointvec = []
+config_fname_pointvec = ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test3_3d.config',
+                         '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test7_proj_rpar100.config'
+                         ]
 #                        ]
 #                         '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test7_proj_rpar20.config',
-#                         '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test7_proj_rpar100.config',]
+#                         ,]
 
-#config_fname_pixvec = []
-config_fname_pixvec = ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test7_angular.config',
-                       '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test8_angular.config',
-                       ]
+config_fname_pixvec = []
+# config_fname_pixvec = ['/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test7_angular.config',
+#                       '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/configs/test8_angular.config',
+#                       ]
 
 for dset_fname in dset_fname_vec:
-    
+
     for config_fname in config_fname_pixvec:
         for jk_id in np.arange(30):
             command_str = "import tricorder; corr = tricorder.PixelCorrelation('" + \
@@ -50,11 +54,11 @@ for dset_fname in dset_fname_vec:
                          "-e", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.err", "python", "-c", command_str])
 
     for config_fname in config_fname_pointvec:
-        for set_str in ['ddd', 'ddr', 'drd', 'rdd', 'rrd', 'rdr', 'drr', 'rrr']:
-            command_str = "import tricorder; corr = tricorder.PointCorrelation('" + \
-                dset_fname + "', '" + config_fname + \
-                "', '" + set_str + "', " + \
-                str(-1) + "); corr.run()"
-            subprocess.call(["bsub", "-W", "47:00", "-R", "rusage[mem=4000]", "-o", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.out",
-                             "-e", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.err", "python", "-c", command_str])
-            
+        for jk_id in np.arange(30):
+            for set_str in ['ddd', 'ddr', 'drd', 'rdd', 'rrd', 'rdr', 'drr', 'rrr']:
+                command_str = "import tricorder; corr = tricorder.PointCorrelation('" + \
+                    dset_fname + "', '" + config_fname + \
+                    "', '" + set_str + "', " + \
+                    str(jk_id) + "); corr.run()"
+                subprocess.call(["bsub", "-W", "47:00", "-R", "rusage[mem=4000]", "-o", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.out",
+                                 "-e", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.err", "python", "-c", command_str])
