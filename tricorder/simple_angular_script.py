0dbad5f71eaf2929e9d9a97b66269f7e5f3aae45
Mike Baumer <mbaumer12@gmail.com>
2018-09-29 21:23:49 -0700

    smaller z bins

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -4,11 +4,11 @@ import paths
 # do3Ds = [True]
 
 if __name__ == '__main__':
-    for i, config_fname in enumerate(['paper13.2', 'paper14.2', 'paper15.2', 'paper13.3', 'paper14.3', 'paper15.3']):
+    for i, config_fname in enumerate(['paper13.1', 'paper14.1', 'paper15.1']):
         do3D = False
         for sigma_z in [0]:
-            for min_z in [.15, .3, .45, .6, .75]:
-                max_z = min_z + 0.15
+            for min_z in [.15, .225, .3, .375, .45, .525, .6, .675, .75, .825]:
+                max_z = min_z + 0.075
                 for rw_scheme in ['ZSPEC', 'ZREDMAGIC']:
                     for dset_id in range(len(paths.dm_y1)):
                         command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_dm(" + str(
@@ -17,9 +17,9 @@ if __name__ == '__main__':
                         subprocess.call(["bsub", "-W", "47:00", "-R", "rusage[mem=4000]", "-o", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs2/%J.out",
                                          "-e", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs2/%J.err", "python", "-c", command_str])
                     for dset_id in range(len(paths.rm_y1)):
-                        for random_oversamp in [.5, 1]:
+                        for random_oversamp in [1]:
                             command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz(" + str(
-                                dset_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + "," + "'ZSPEC','Z',"+str(random_oversamp)+")"
+                                dset_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'"+rw_scheme+"','Z',"+str(random_oversamp)+")"
                             print command_str
                             subprocess.call(["bsub", "-W", "47:00", "-R", "rusage[mem=4000]", "-o", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.out",
                                              "-e", "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new2/logs/%J.err", "python", "-c", command_str])
