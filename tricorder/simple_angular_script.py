b906bb62d897ef51f84cb4a3108f056cbddb457b
mbaumer <mbaumer12@gmail.com>
2019-03-16 07:46:32 -0700

    run new halos script

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
             for min_z in [.15, .3, .45, .6, .75]:
                 max_z = min_z + 0.15
                 for rw_scheme in ['Z']:
-                    for dset_id in [0, 1, 2, 3, 5, 21]:
+                    for dset_id in [0, 1, 2, 3]:
                         for random_oversamp in [3]:
                             command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_halos(" + str(
                                 dset_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'"+rw_scheme+"','Z',"+str(random_oversamp)+")"
