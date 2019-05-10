6629acce63ac47cf4e9a7006bd392f71b2ec57e4
Mike Baumer <mbaumer12@gmail.com>
2019-05-10 00:41:15 -0700

    do normal gal jk regions

--- tricorder/simple_angular.py
+++ tricorder/simple_angular.py
@@ -460,11 +460,11 @@ def calc_3pt_noisy_photoz(dset_id, jk_id, config_fname, do3D, min_z, max_z, sigm
     jk_classifier = pickle.load( open( "/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new3/jk_classifiers/buzzard_jk.pkl", "rb" ) )
     data_inds = jk_classifier.predict(zip(data_slice[ra_var],data_slice[dec_var]))
     random_inds = jk_classifier.predict(zip(randoms_slice[ra_var],randoms_slice[dec_var]))
-    data_slice = data_slice[data_inds == jk_id]
-    randoms_slice = randoms_slice[random_inds == jk_id]
+    data_slice = data_slice[data_inds != jk_id]
+    randoms_slice = randoms_slice[random_inds != jk_id]
 
     file_name = config_fname + \
-        '_newbuzzardrmdset'+str(dset_id)+'_jk'+str(jk_id)+'_sigma'+str(sigma_z) + \
+        '_newbuzzardrm2dset'+str(dset_id)+'_jk'+str(jk_id)+'_sigma'+str(sigma_z) + \
         '_'+str(zvar)+'_'+str(min_z)+'_'+str(max_z) + \
         '_rsamp'+str(random_oversamp)
         
