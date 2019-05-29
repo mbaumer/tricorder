fc2be25119499c83382bdaece369eebf5918656c
Mike Baumer <mbaumer12@gmail.com>
2019-05-28 19:12:46 -0500

    fix typo

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         for sigma_z in [0]:
             for min_z in [.15, .3, .45, .6, .75]:
                 max_z = min_z + 0.15
-                for random_oversamp in [10,20]:
+                for random_oversamp in [10, 20]:
                     for jk_id in [-1]:
                         for outvar in ['ddd', 'drr', 'rdr', 'rrd', 'rrr']:
                             for rw_scheme in ['ZREDMAGIC']:
@@ -23,23 +23,23 @@ if __name__ == '__main__':
                                 #print command_str
                                 # subprocess.call(["bsub", "-W", "47:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
                                 #                 "-e", errlogpath, "python", "-c", command_str])
-                                for dm_oversamp in [12]:
+                                # for dm_oversamp in [12]:
                                     # command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_MICEdm(" + str(
                                     #     dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'redshift','Z',"+str(dm_oversamp)+","+str(random_oversamp)+", '"+rw_scheme+"', outvar='"+outvar+"')"
                                     # print command_str
                                     # subprocess.call(["bsub", "-W", "47:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
                                     #                  "-e", errlogpath, "python", "-c", command_str])
 
-                                    if jk_id == -1:
-                                        dset_ids = range(len(paths.dm_y1))
-                                    else:
-                                        dset_ids = [primary_dset_id]
-                                    for dset_id in dset_ids:
-                                        command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_dm(" + str(
-                                            dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'redshift','Z',"+str(dm_oversamp)+","+str(random_oversamp)+", '"+rw_scheme+"', outvar='"+outvar+"')"
-                                        print command_str
-                                        subprocess.call(["bsub", "-W", "24:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
-                                                         "-e", errlogpath, "python", "-c", command_str])
+                                    # if jk_id == -1:
+                                    #     dset_ids = range(len(paths.dm_y1))
+                                    # else:
+                                    #     dset_ids = [primary_dset_id]
+                                    # for dset_id in dset_ids:
+                                    #     command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_dm(" + str(
+                                    #         dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'redshift','Z',"+str(dm_oversamp)+","+str(random_oversamp)+", '"+rw_scheme+"', outvar='"+outvar+"')"
+                                    #     print command_str
+                                    #     subprocess.call(["bsub", "-W", "24:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
+                                    #                      "-e", errlogpath, "python", "-c", command_str])
 
                                 #     if min_z != .45:
                                 #         continue
@@ -64,10 +64,10 @@ if __name__ == '__main__':
                                     command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz(" + str(
                                         dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'"+rw_scheme+"','Z',"+str(random_oversamp)+", outvar='"+outvar+"')"
                                     print command_str
-                                    subprocess.call(["bsub", "-W", "12s:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
+                                    subprocess.call(["bsub", "-W", "12:00", "-n", ncpus, "-R", "span[hosts=1]", "-o", outlogpath,
                                                      "-e", errlogpath, "python", "-c", command_str])
 
-                                #Y3
+                                # Y3
                                 # if jk_id == -1:
                                 #     dset_ids = range(len(paths.rm_y3))
                                 # else:
