639c81b319c6cac64c806cd300dfb3858f260f5a
Mike Baumer <mbaumer12@gmail.com>
2019-06-28 19:36:50 -0500

    add cpu time

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -10,7 +10,7 @@ primary_dset_id = 0
 if __name__ == '__main__':
     for i, config_fname in enumerate(['newpaper14.1']):
         do3D = False
-        for sigma_z in [0.005, .01, .02, .03]:
+        for sigma_z in [0, 0.005, .01, .02, .03]:
             for min_z in [.15, .3, .45, .6]:
                 max_z = min_z + 0.15
                 for random_oversamp in [20]:
@@ -43,7 +43,7 @@ if __name__ == '__main__':
                                         command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz_dm(" + str(
                                             dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'redshift','Z',"+str(dm_oversamp)+","+str(random_oversamp)+", '"+rw_scheme+"', outvar='"+outvar+"')"
                                         print command_str
-                                        subprocess.call(["bsub", "-W", "18:00", "-n", ncpus, "-C", "1", "-R", "span[hosts=1]", "-o", outlogpath,
+                                        subprocess.call(["bsub", "-W", "47:00", "-n", ncpus, "-C", "1", "-R", "span[hosts=1]", "-o", outlogpath,
                                                          "-e", errlogpath, "python", "-c", command_str])
 
                                 #     if min_z != .45:
@@ -61,10 +61,10 @@ if __name__ == '__main__':
                                 #                          "-e", errlogpath, "python", "-c", command_str])
 
                                 # RMY1
-                                # if jk_id == -1:
-                                #    dset_ids = range(len(paths.rm_y1))
-                                # else:
-                                dset_ids = [primary_dset_id]
+                                if jk_id == -1:
+                                    dset_ids = range(len(paths.rm_y1))
+                                else:
+                                    dset_ids = [primary_dset_id]
                                 for dset_id in dset_ids:
                                     command_str = "import simple_angular; simple_angular.calc_3pt_noisy_photoz(" + str(
                                         dset_id) + ", " + str(jk_id) + ", '" + config_fname + "', "+str(do3D)+", " + str(min_z) + "," + str(max_z) + "," + str(sigma_z) + ",'"+rw_scheme+"','Z',"+str(random_oversamp)+", outvar='"+outvar+"')"
