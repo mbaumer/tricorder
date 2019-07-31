862de22c1b40d572266cffbcd815fc8e2e59bd76
Mike Baumer <mbaumer12@gmail.com>
2019-07-31 15:25:30 -0600

    rest of figs except evol

--- tricorder/make_figures.py
+++ tricorder/make_figures.py
@@ -17,9 +17,9 @@ from astropy.io import fits
 import paths
 import skymapper as skm
 
-matplotlib.rcParams.update({'font.size': 18})
-matplotlib.rc('xtick', labelsize=14) 
-matplotlib.rc('ytick', labelsize=14) 
+matplotlib.rcParams.update({'font.size': 14})
+matplotlib.rc('xtick', labelsize=12) 
+matplotlib.rc('ytick', labelsize=12) 
 matplotlib.rc('font', family='serif')
 
 def make_easy_figures():
@@ -77,6 +77,116 @@ def make_easy_figures():
 
     plt.savefig('./figures/redmagic_errors.pdf',dpi=300,bbox_inches='tight')
     plt.savefig('./figures/redmagic_errors.png',dpi=300,bbox_inches='tight')
+
+def make_triangle_diagrams():
+    colors = palettable.colorbrewer.qualitative.Paired_5.hex_colors
+    def drawTriangleScatter(min_r,max_r,min_u,max_u,min_v,max_v,color=0,num=20):
+        test = np.array(np.meshgrid(np.linspace(min_r,max_r,num=num),
+                np.linspace(min_u,max_u,num=num),
+                np.linspace(min_v,max_v,num=10*num),))
+        d2 = test[0,:,:,:]
+        d3 = test[0,:,:,:]*test[1,:,:,:]
+        d1 = np.abs(test[2,:,:,:])*d3+d2 
+        cosine = (d2**2 + d3**2 - d1**2)/(2*d2*d3+1e-9)
+        sine = np.sqrt(1-cosine**2)
+        points = d3*cosine,d3*sine
+        plt.scatter(np.array(points).reshape(2,-1)[0,:],np.array(points).reshape(2,-1)[1,:],color=colors[color],alpha=.1,marker='.')
+        plt.plot([0,20],[0,0],color='k')
+        plt.plot([0,-4,20],[0,12,0],color='k',linestyle='--')
+        plt.plot(20,0,color='r',marker='s')
+        plt.xlim(-30,25)
+        plt.ylim(-5,25)
+
+    drawTriangleScatter(19.9,20.1,.3,1,0,.2, color=0,num=50)
+    drawTriangleScatter(19.9,20.1,.3,1,.2,.4,color=1,num=50)
+    drawTriangleScatter(19.9,20.1,.3,1,.4,.6,color=2,num=50)
+    drawTriangleScatter(19.9,20.1,.3,1,.6,.8,color=3,num=50)
+    drawTriangleScatter(19.9,20.1,.3,1,.8,1, color=4,num=100)
+    plt.xlabel('RA (arcmin)')
+    plt.ylabel('DEC (arcmin)')   
+
+    plt.savefig('./figures/perfect_binning.pdf',dpi=300,bbox_inches='tight')
+    plt.savefig('./figures/perfect_binning.png',dpi=300,bbox_inches='tight')
+
+    plt.figure() 
+
+    drawTriangleScatter(16,24,.3,1,0,.2, color=0,num=50)
+    drawTriangleScatter(16,24,.3,1,.2,.4,color=1,num=50)
+    drawTriangleScatter(16,24,.3,1,.4,.6,color=2,num=50)
+    drawTriangleScatter(16,24,.3,1,.6,.8,color=3,num=50)
+    drawTriangleScatter(16,24,.3,1,.8,1, color=4,num=120)    
+
+    plt.savefig('./figures/tolerance_20.pdf',dpi=300,bbox_inches='tight')
+    plt.savefig('./figures/tolerance_20.png',dpi=300,bbox_inches='tight')
+
+
+def make_covariance_fig():
+
+    zmin = .45
+    zmax = zmin+.15
+    data = plottools.load_res_indep(path, 'dm','newpaper13.1','ZSPEC',zmin,zmax,'12x20',sigma=0)
+    data2 = plottools.load_res_indep(path,'dm','newpaper13.1','ZREDMAGIC',zmin,zmax,'12x20',sigma=0)
+    galdata = plottools.load_res_indep(path, 'newbuzzardrm2','newpaper13.1','ZSPEC',zmin,zmax,'20',sigma=0)
+    galdata2 = plottools.load_res_indep(path,'newbuzzardrm2','newpaper13.1','ZREDMAGIC',zmin,zmax,'20',sigma=0)
+    plt.figure()
+    fig,axarr = plt.subplots(2,2,figsize=(6,6))
+    
+    vmin= 0
+    vmax= 1
+    
+    axarr[0,0].imshow(np.corrcoef(plottools.compress_dv(galdata['Q'].values.reshape(-1,10)).T),vmin=vmin,vmax=vmax,origin='lower',extent=(0,1,0,1))
+    axarr[0,0].set_title(r'Galaxies: True n(z)')
+    axarr[0,0].set_xticks([.1,.3,.5,.7,.9])
+    axarr[0,0].set_yticks([.1,.3,.5,.7,.9])
+    #axarr[0,0].set_xlabel('v')
+    axarr[0,0].set_ylabel('v')
+    axarr[0,1].imshow(np.corrcoef(plottools.compress_dv(galdata2['Q'].values.reshape(-1,10)).T),vmin=vmin,vmax=vmax,origin='lower',extent=(0,1,0,1))
+    axarr[0,1].set_title(r'Galaxies: RedMaGiC n(z)')
+    #axarr[0,1].set_xlabel('v')
+    #axarr[0,1].set_ylabel('v')
+    axarr[0,1].set_xticks([.1,.3,.5,.7,.9])
+    axarr[0,1].set_yticks([.1,.3,.5,.7,.9])
+    axarr[1,0].imshow(np.corrcoef(plottools.compress_dv(data['Q'].values.reshape(-1,10)).T),vmin=vmin,vmax=vmax,origin='lower',extent=(0,1,0,1))
+    axarr[1,0].set_title(r'DM: True n(z)')
+    axarr[1,0].set_xticks([.1,.3,.5,.7,.9])
+    axarr[1,0].set_yticks([.1,.3,.5,.7,.9])
+    axarr[1,0].set_xlabel('v')
+    axarr[1,0].set_ylabel('v')
+    im = axarr[1,1].imshow(np.corrcoef(plottools.compress_dv(data2['Q'].values.reshape(-1,10)).T),vmin=vmin,vmax=vmax,origin='lower',extent=(0,1,0,1))
+    axarr[1,1].set_title(r'DM: RedMaGiC n(z)')
+    axarr[1,1].set_xlabel('v')
+    #axarr[1,1].set_ylabel('v')
+    axarr[1,1].set_xticks([.1,.3,.5,.7,.9])
+    axarr[1,1].set_yticks([.1,.3,.5,.7,.9])
+    
+    #fig.colorbar(im, ax=axarr.ravel().tolist(),shrink=.4,aspect=10)
+    #plt.suptitle(str(zmin)+r'$<z<$'+str(zmax))
+    plt.tight_layout()
+    cbar_ax = fig.add_axes([.97, 0.2, .04, 0.6])
+    fig.colorbar(im, cax=cbar_ax, orientation="vertical")
+
+    plt.savefig('./figures/corrmat_examples.pdf',dpi=300,bbox_inches='tight')
+    plt.savefig('./figures/corrmat_examples.png',dpi=300,bbox_inches='tight') 
+
+def make_data_vector_plots():
+
+    path = '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new3/photoz_syst/'
+
+    for i,zmin in enumerate([.15, .3, .45, .6]):
+        zmax = zmin+.15
+        data =  plottools.load_res_indep(path,'dm','newpaper13.1','ZSPEC',zmin,zmax,'12x20',sigma=0)
+        galdata = plottools.load_res_indep(path,'newbuzzardrm2','newpaper13.1','ZSPEC',zmin,zmax,'20',sigma=0)
+        plt.figure()
+        plottools.plot_dv(data,'Q', offset=0.,indiv_runs=True,color='Blue',label=r'Dark Matter',compressed=True)
+        plottools.plot_dv(galdata,'Q', offset=0.,indiv_runs=True,color='Red',label='Galaxies',compressed=True)
+        plt.xlabel('v')
+        plt.ylabel('Q')
+        plt.title(str(zmin)+r'$<z<$'+str(zmax))
+        plt.legend() 
+
+        plt.savefig('./figures/spec_bin'+str(i+1)+'_dv.pdf',dpi=300,bbox_inches='tight')
+        plt.savefig('./figures/spec_bin'+str(i+1)+'_dv.png',dpi=300,bbox_inches='tight')
+        plt.figure()
 
 
 def get_zspec(is11k=False):
@@ -190,8 +300,8 @@ def get_zspec_both():
         axarr = figure.get_axes()
         plt.suptitle(str(zmin)+r'$ < z < $'+str(zmax))
         plt.legend()
-        plt.savefig('./figures/spec_bin'+str(i+1)+str11k+'.pdf',dpi=300,bbox_inches='tight')
-        plt.savefig('./figures/spec_bin'+str(i+1)+str11k+'.png',dpi=300,bbox_inches='tight')
+        plt.savefig('./figures/both_bin'+str(i+1)+'.pdf',dpi=300,bbox_inches='tight')
+        plt.savefig('./figures/both_bin'+str(i+1)+'.png',dpi=300,bbox_inches='tight')
 
 def get_zspec_zrm(is11k=False):
     path = '/nfs/slac/des/fs1/g/sims/mbaumer/3pt_sims/new3/photoz_syst/'
@@ -399,14 +509,16 @@ def get_gaussian_photoz(is11k=False):
 
 if __name__ == '__main__':
     #make_easy_figures()
+    make_data_vector_plots()
+    make_covariance_fig()
     get_zspec_both()
     #get_zspec()
     #get_zspec(is11k=True)
-    #get_zspec_zrm()
-    #get_zspec_zrm(is11k=True)
-    #get_gaussian_photoz()
-    #get_gaussian_photoz(is11k=True)
-    #get_tolerance_figs()
-    #get_tolerance_figs(is11k=True)
-    #get_u_figs()
-    #get_u_figs(is11k=True)
+    get_zspec_zrm()
+    get_zspec_zrm(is11k=True)
+    get_gaussian_photoz()
+    get_gaussian_photoz(is11k=True)
+    get_tolerance_figs()
+    get_tolerance_figs(is11k=True)
+    get_u_figs()
+    get_u_figs(is11k=True)
