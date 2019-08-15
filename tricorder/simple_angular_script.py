9c323352c107f50f52b045af6ed70654f675de48
Mike Baumer <mbaumer12@gmail.com>
2019-08-15 16:50:26 -0600

    try just doing galaxies

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -70,17 +70,17 @@ if __name__ == '__main__':
 
                             for rw_scheme in zlist:
 
-                                for dm_oversamp in [10]:
+                                # for dm_oversamp in [10]:
 
-                                    if jk_id == -1:
-                                        dset_ids = range(len(paths.dm_y1))
-                                    else:
-                                        dset_ids = [primary_dset_id]
-                                    for dset_id in dset_ids:
+                                #     if jk_id == -1:
+                                #         dset_ids = range(len(paths.dm_y1))
+                                #     else:
+                                #         dset_ids = [primary_dset_id]
+                                #     for dset_id in dset_ids:
 
-                                        nice_job_submit(do3Ds[i], outvar, config_fname, 'dm', dset_id,
-                                                        jk_id, sigma_z, rw_scheme, min_z, max_z, random_oversamp,
-                                                        dm_oversamp=dm_oversamp)
+                                #         nice_job_submit(do3Ds[i], outvar, config_fname, 'dm', dset_id,
+                                #                         jk_id, sigma_z, rw_scheme, min_z, max_z, random_oversamp,
+                                #                         dm_oversamp=dm_oversamp)
 
                                 # RMY1
                                 if jk_id == -1:
