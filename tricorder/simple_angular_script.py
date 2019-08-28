0b87be70cfb58f6f5caff663ec4eef8b5f72e8f8
Mike Baumer <mbaumer12@gmail.com>
2019-08-27 22:49:54 -0600

    3d systematics

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -56,7 +56,7 @@ def nice_job_submit(do3D, outvar, config_fname, dset_flavor, dset_id, jk_id, sig
 
 
 if __name__ == '__main__':
-    for i, config_fname in enumerate(['paper_3dval4']):
+    for i, config_fname in enumerate(['fiducial3d_halfu','fiducial3d_75u','fiducial3d_tolup','fiducial3d_toldown','fiducial3d_25Mpc','fiducial3d_35Mpc']):
         #do3D = False
         for z_width in [0.15]:
             for sigma_z in [0]:
@@ -66,7 +66,7 @@ if __name__ == '__main__':
                         for jk_id in [-1]:
                             for outvar in ['ddd', 'rrr', 'drr', 'rdr', 'rrd']:
                                 if sigma_z == 0:
-                                    zlist = ['ZSPEC', 'ZREDMAGIC']
+                                    zlist = ['ZSPEC']
                                 else:
                                     zlist = ['ZSPEC']
 
