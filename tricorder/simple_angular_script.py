4309b205e91d8284fd1d8df055a48c5e8f7e289d
Mike Baumer <mbaumer12@gmail.com>
2019-09-07 00:32:09 -0600

    3d systematics redmagic

--- tricorder/simple_angular_script.py
+++ tricorder/simple_angular_script.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
                                                             jk_id, sigma_z, rw_scheme, min_z, max_z, random_oversamp,
                                                             dm_oversamp=dm_oversamp)
 
-                                    RMY1
+                                    # RMY1
                                     if jk_id == -1:
                                         dset_ids = range(len(paths.rm_y1))
                                     else:
